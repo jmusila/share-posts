c3105cd80081a75d4572bfcc7ec14b96222940dd
jmusila <jonathanmusila6@gmail.com>
2021-07-09 15:25:23 +0300

    added validation

--- app/controllers/Users.php
+++ app/controllers/Users.php
@@ -10,6 +10,22 @@ class Users extends Controller
     {
         // Check for POST
         if ($_SERVER['REQUEST_METHOD'] == 'POST') {
+
+            // Sanitize post data
+            $_POST = filter_input_array(INPUT_POST, FILTER_SANITIZE_STRING);
+
+            $data = [
+                'name' => trim($_POST['name']),
+                'email' => trim($_POST['email']),
+                'password' => trim($_POST['password']),
+                'confirm_password' => trim($_POST['confirm_password']),
+                'name_error' => '',
+                'email_error' => '',
+                'password_error' => '',
+                'confirm_password_error' => ''
+            ];
+
+
         } else {
             $data = [
                 'name' => '',
@@ -41,6 +57,34 @@ class Users extends Controller
 
             //Load view
             $this->view('users/login', $data);
+        }
+    }
+
+    /**
+     * Validate User
+     */
+    public function validateUser($data)
+    {
+        if(empty($data['name'])){
+            $data['name_error'] = 'The name field is required.';
+        }
+
+        if(empty($data['email'])){
+            $data['email_error'] = 'The email field is required.';
+        }elseif(!filter_var($data['email'], FILTER_VALIDATE_EMAIL)){
+            $data['email_error'] = 'Please enter a valid email address.';
+        }
+
+        if(empty($data['password'])){
+            $data['password_error'] = 'The password field is required.';
+        }elseif(strlen($data['password']) < 6){
+            $data['password_error'] = 'The password must be atleast 6 characters.';
+        }
+
+        if(empty($data['confirm_password'])){
+            $data['confirm_password_error'] = 'The confirm password field is required.';
+        }elseif(!$data['password'] == $data['confirm_password']){
+
         }
     }
 }
