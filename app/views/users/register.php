7f93bd0e8b1b1a9305d1bd8a20db4b963d02bc08
jmusila <jonathanmusila6@gmail.com>
2021-07-09 13:08:39 +0300

    added the register form

--- app/views/users/register.php
+++ app/views/users/register.php
@@ -1,3 +1,9 @@
 <?php require APPROOT . '/views/includes/header.php'; ?>
-    
+    <div class="row">
+        <div class="col-md-6 mx-auto">
+            <div class="card card-body bg-lt mt-5">
+                <h2>Register New User</h2>
+            </div>
+        </div>
+    </div>
 <?php require APPROOT . '/views/includes/footer.php'; ?>
