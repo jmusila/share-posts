b11e3f4ca3153fd7bd7c5a8682dfe86fc7739490
jmusila <jonathanmusila6@gmail.com>
2021-07-09 12:02:27 +0300

    added changes

--- app/views/pages/index.php
+++ app/views/pages/index.php
@@ -1,4 +1,4 @@
 <?php require APPROOT . '/views/includes/header.php'; ?>
 <h1> <?php echo $data['title'];?></h1>
-    <p>This is a PHP Framework. Please refer to the .README.Md on how to use the Framework</p>
+    
 <?php require APPROOT . '/views/includes/footer.php'; ?>
