043a20e596755cd2f25a68c38683192d10179694
jmusila <jonathanmusila6@gmail.com>
2021-07-14 09:23:52 +0300

    added session helper

--- app/helpers/session_helper.php
+++ app/helpers/session_helper.php
@@ -8,17 +8,23 @@ session_start();
  * Example: flash('register_success', 'User successfully created', 'alert alert-danger');
  * Diplay: <?php echo flash('register_success');
  */
-function flash($name = '', $message = '', $class = 'alert alert-success'){
-    if(!empty($name)){
-        if(!empty($message) && empty($_SESSION[$name])){
-            if(!empty($_SESSION[$name])){
+function flash($name = '', $message = '', $class = 'alert alert-success')
+{
+    if (!empty($name)) {
+        if (!empty($message) && empty($_SESSION[$name])) {
+            if (!empty($_SESSION[$name])) {
                 unset($_SESSION[$name]);
             }
-            if(!empty($_SESSION[$name . '_class'])){
+            if (!empty($_SESSION[$name . '_class'])) {
                 unset($_SESSION[$name . '_class']);
             }
             $_SESSION[$name] = $name;
             $_SESSION[$name . '_class'] = $class;
+        } elseif (empty($message) && !empty($_SESSION[$name])) {
+            $class = !empty($_SESSION[$name . '_class']) ? $_SESSION[$name . '_class'] : '';
+            echo '<div class = "'.$class.'" id="msg-flash">'.$_SESSION[$name].'</div>';
+            unset($_SESSION[$name]);
+            unset($_SESSION[$name . '_class']);
         }
     }
 }
