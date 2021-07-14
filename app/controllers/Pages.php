f2da206ad893b4ec6ee67e6da78bdb9bd4a841fa
jmusila <jonathanmusila6@gmail.com>
2021-07-14 15:51:21 +0300

    added get posts

--- app/controllers/Pages.php
+++ app/controllers/Pages.php
@@ -8,6 +8,10 @@ class Pages extends Controller
 
     public function index()
     {
+        if(isLoggedIn()){
+            redirect('posts');
+        }
+        
         $data = [
             'title' => 'SharePosts',
             'description' => 'Simple Social Network buil on the Custom Made PHP Framework.'
--- app/controllers/Posts.php
+++ app/controllers/Posts.php
@@ -27,4 +27,25 @@ class Posts extends Controller
 
         $this->view('posts/index', $data);
     }
+    /**
+     * Create Posts
+     */
+    public function add()
+    {
+        $data = $this->postData();
+
+        $this->view('posts/add', $data);
+    }
+
+    public function postData()
+    {
+        $data = [
+            'title' => '',
+            'body' => '',
+            'created_at' => date('Y-m-d h:i:s', time()),
+            'updated_at' => date('Y-m-d h:i:s', time())
+        ];
+
+        return $data;
+    }
 }
