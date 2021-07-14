6e27c1ac53da9ed8d58444448dc7df3725415dac
jmusila <jonathanmusila6@gmail.com>
2021-07-14 08:54:14 +0300

    added timestamps

--- app/helpers/helper.php
+++ app/helpers/helper.php
@@ -2,22 +2,10 @@
 
 /**
  * Simple page redirect function
+ *
+ * @return void
  */
 function redirect($page)
 {
     header('location: ' . URLROOT . '/' . $page);
-}
-
-/**
- * Automatically add timestamps
- */
-function timestamps($data)
-{
-    if($data['created_at']){
-        $data['created_at'] = date('Y-m-d h:i:sa', time());
-    }
-
-    if($data['updated_at']){
-        $data['updated_at'] = date('Y-m-d h:i:sa', time());
-    }
 }
