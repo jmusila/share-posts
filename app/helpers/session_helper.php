b4afbf4ad927b36dd96898febba38ce7a2a28a75
jmusila <jonathanmusila6@gmail.com>
2021-07-14 09:19:00 +0300

    added session helper

--- app/helpers/session_helper.php
+++ app/helpers/session_helper.php
@@ -11,6 +11,12 @@ session_start();
 function flash($name = '', $message = '', $class = 'alert alert-success'){
     if(!empty($name)){
         if(!empty($message) && empty($_SESSION[$name])){
+            if(!empty($_SESSION[$name])){
+                unset($_SESSION[$name]);
+            }
+            if(!empty($_SESSION[$name . '_class'])){
+                unset($_SESSION[$name . '_class']);
+            }
             $_SESSION[$name] = $name;
             $_SESSION[$name . '_class'] = $class;
         }
