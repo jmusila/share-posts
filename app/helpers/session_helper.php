6ac4fd4ec76c8327e7970bb4981157c3d8488f9b
jmusila <jonathanmusila6@gmail.com>
2021-07-14 09:14:54 +0300

    added session helper

--- app/helpers/session_helper.php
+++ app/helpers/session_helper.php
@@ -1,2 +1,18 @@
 <?php
 
+session_start();
+
+/**
+ * Flash messaging helper
+ *
+ * Example: flash('register_success', 'User successfully created', 'alert alert-danger');
+ * Diplay: <?php echo flash('register_success');
+ */
+function flash($name = '', $message = '', $class = 'alert alert-success'){
+    if(!empty($name)){
+        if(!empty($message) && empty($_SESSION[$name])){
+            $_SESSION[$name] = $name;
+            $_SESSION[$name . '_class'] = $class;
+        }
+    }
+}
