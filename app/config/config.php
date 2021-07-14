20d0fcb13e2cdd5c6ca53a27a389c6e06ffb7934
jmusila <jonathanmusila6@gmail.com>
2021-07-14 16:24:58 +0300

    added get posts

--- app/config/config.php
+++ app/config/config.php
@@ -7,10 +7,10 @@
 // $dotenv->load();
 
 /** DB Params */
-define('DB_HOST', 'localhost');
-define('DB_USER', 'myuser');
-define('DB_PASS', '123Me!@#');
-define('DB_NAME', 'share_posts');
+define('DB_HOST', 'DB_HOST');
+define('DB_USER', 'DB_USER');
+define('DB_PASS', 'DB_PASS');
+define('DB_NAME', 'DB_NAME');
 
 /** App root */
 define('APPROOT', dirname(dirname(__FILE__)));
