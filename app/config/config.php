4c091b2116fcee2b43e75d1cbdf350ad26f7de4f
jmusila <jonathanmusila6@gmail.com>
2021-07-15 10:34:23 +0300

    added .env configurations

--- app/config/config.php
+++ app/config/config.php
@@ -1,16 +1,14 @@
 <?php
 
-// require_once "../libraries/DotEnv.php";
+require_once '../app/libraries/DotEnv.php';
 
-// $dotenv = new DotEnv(__DIR__ . "/.env");
-
-// $dotenv->load();
+(new DotEnv(__DIR__ . '/.env'))->load();
 
 /** DB Params */
-define('DB_HOST', 'DB_HOST');
-define('DB_USER', 'DB_USER');
-define('DB_PASS', 'DB_PASS');
-define('DB_NAME', 'DB_NAME');
+define('DB_HOST', getenv('DB_HOST'));
+define('DB_USER', getenv('DB_USERNAME'));
+define('DB_PASS', getenv('DB_PASSWORD'));
+define('DB_NAME', getenv('DB_DATABASE'));
 
 /** App root */
 define('APPROOT', dirname(dirname(__FILE__)));
