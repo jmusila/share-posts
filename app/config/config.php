c60925648a952b0b265a97ffa11419475802eda5
jmusila <jonathanmusila6@gmail.com>
2021-07-15 13:02:32 +0300

    added app url

--- app/config/config.php
+++ app/config/config.php
@@ -9,13 +9,15 @@ define('DB_HOST', getenv('DB_HOST'));
 define('DB_USER', getenv('DB_USERNAME'));
 define('DB_PASS', getenv('DB_PASSWORD'));
 define('DB_NAME', getenv('DB_DATABASE'));
+
+/** Time and Timezone */
 define('APP_TIMEZONE', getenv('APP_TIMEZONE'));
 
 /** App root */
 define('APPROOT', dirname(dirname(__FILE__)));
 
 /**URL root */
-define('URLROOT', 'http://shareposts.com');
+define('URLROOT', getenv('APP_URL'));
 
 /** Site Name */
 define('SITENAME', getenv('APP_NAME'));
